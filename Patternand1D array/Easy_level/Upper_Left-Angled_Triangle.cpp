c03e214fb88eabf73c099a69ae60f75733d37167
POOVEBDHURAN <poovendhiran1901@gmail.com>
2024-08-04 19:57:24 +0530

    Create Upper_Left-Angled_Triangle.cpp

---
+++ Patternand1D array/Easy_level/Upper_Left-Angled_Triangle.cpp
@@ -0,0 +1,16 @@
+/*Print an Upper Left-Angled Triangle Pattern of Stars*/
+#include <iostream>
+class Solution {
+public:
+ static void printSquarePattern(int n) {
+ int i,j;
+ for(int i=0;i<n;i++,printf("\n")){
+ for(j=0;j<n-i;j++) std::cout<<" * ";
+ }
+ }
+};
+int main() {
+ int n = 4; // Example input
+ Solution::printSquarePattern(n);
+ return 0;
+}
